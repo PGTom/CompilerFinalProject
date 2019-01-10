f459f79cfbd6cc08665f4311402d7d0b90825f2e
changchiyou <46549482+changchiyou@users.noreply.github.com>
2019-01-10 21:28:37 +0800

    Update compile_CompAndRun.bat

--- compile_CompAndRun.bat
+++ compile_CompAndRun.bat
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-read -p "File Name:" name
-
 #copile bision
-bison -d -o y.tab.c ${name}.y 2> /dev/null
-gcc -c -g -I.. y.tab.c 2> /dev/null
+bison -d -o final.tab.c final.y 2> /dev/null
+gcc -c -g -I.. final.tab.c 2> /dev/null
 #compile flex
-flex -o lex.yy.c ${name}.l 2> /dev/null
-gcc -c -g -I.. lex.yy.c 2> /dev/null
+flex -o final.yy.c final.l 2> /dev/null
+gcc -c -g -I.. final.yy.c 2> /dev/null
 #compile and link bison and flex
-gcc -o ${name} y.tab.o lex.yy.o -ll 2> /dev/null
-
-#run 
-./${name}
+gcc -o lispFile final.tab.o final.yy.o -ll 2> /dev/null
 
